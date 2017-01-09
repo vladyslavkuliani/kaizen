388d2f012284ea383a1f84199ccb34c7c49ab320
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2017-01-09 14:40:41 -0800

    Close to finalization of Auth & Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_managers
+    @current_managers ||= Manager.find(session[:manager_id]) if session[:manager_id]
+  end
+  helper_method :current_managers
+
+  def authorize
+    redirect_to '/login' unless current_managers
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    managers = Manager.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if managers && managers.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user
+      # logged in when they navigate around our website.
+      session[:manager_id] = managers.id
+      redirect_to '/'
+    else
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:manager_id] = nil
+    redirect_to '/login'
   end
 
 end
