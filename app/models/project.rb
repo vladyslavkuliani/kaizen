c2a8cf2106617030d77035bc43ca233f067dc7fe
margarh1 <ha.margaret1@gmail.com>
2017-01-09 15:18:32 -0800

    added association between project and tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   belongs_to :manager
+  has_many :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109224959) do
+ActiveRecord::Schema.define(version: 20170109231627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20170109224959) do
     t.integer  "priority_level"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "project_id"
+    t.index ["project_id"], name: "index_tasks_on_project_id", using: :btree
   end
 
   create_table "taskskills", force: :cascade do |t|
@@ -78,4 +80,5 @@ ActiveRecord::Schema.define(version: 20170109224959) do
   end
 
   add_foreign_key "projects", "managers"
+  add_foreign_key "tasks", "projects"
 end
