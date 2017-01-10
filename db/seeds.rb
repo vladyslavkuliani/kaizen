8b6a3e824d142999ac7cd1aae51bf40774db26fa
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2017-01-10 10:41:33 -0800

    add ffaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+50.times do
+  Skill.create(name:FFaker::Skill.tech_skills)
+end
