4bf947c021b583e885131a98ce7002c5c53d4503
margarh1 <ha.margaret1@gmail.com>
2017-01-10 11:15:24 -0800

    created ability to edit and delete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,18 +16,32 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find_by_title(task)
+    @project = Project.find(@task.project_id)
   end
 
   def update
+    updated_task = Task.find_by_title(task)
+    params[:skills].each do |skill|
+      updated_task.skills << Skill.find(skill)
+    end
+    if updated_task.update(task_params)
+      redirect_to project_path(Project.find(updated_task.project_id).title)
+    else
+      flash[:error] = updated_task.errors.full_messages.join("\n")
+      redirect_to edit_task_path(task.title)
+    end
   end
 
   def destroy
+    Task.destroy(task)
+    redirect_to projects_path
   end
 
   private
 
-  def project
-    params[:name]
+  def task
+    params[:title]
   end
 
   def task_params
