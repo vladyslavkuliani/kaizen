83555cf18e6067c5949410038d2b25fa62229550
sophieluo <sophie@groobusiness.com>
2017-01-13 09:20:45 -0800

    change mailer url

--- app/mailers/developer_mailer.rb
+++ app/mailers/developer_mailer.rb
@@ -2,8 +2,8 @@ class DeveloperMailer < ApplicationMailer
 
   def notice_email(developer)
    @developer = developer
-   @url  = 'http://localhost:3000/login'
+   @url  = 'http://desolate-spire-13116.herokuapp.com/'
    mail(to: @developer.email, subject: 'You are assigned a new task!')
   end
-  
+
 end
